34fb0c675842c989f364ae4d814ac647928d6118
Regis Freyd <djaiss@users.noreply.github.com>
2026-07-19 22:03:11 -0400

    feat: add the pricing page to the marketing site (#144)

--- routes/marketing.php
+++ routes/marketing.php
@@ -5,10 +5,13 @@ declare(strict_types=1);
 use App\Http\Controllers\Marketing\Docs\ApiDocsController;
 use App\Http\Controllers\Marketing\Docs\ApiDocsMarkdownController;
 use App\Http\Controllers\Marketing\MarketingController;
+use App\Http\Controllers\Marketing\PricingController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['marketing'])->group(function (): void {
     Route::get('/', [MarketingController::class, 'index'])->name('marketing.index');
+
+    Route::get('pricing', [PricingController::class, 'index'])->name('marketing.pricing.index');
 
     Route::get('docs/api', [ApiDocsController::class, 'index'])->name('marketing.docs.api.index');
     Route::get('docs/api.md', [ApiDocsMarkdownController::class, 'index'])->name('marketing.docs.api.markdown.index');
